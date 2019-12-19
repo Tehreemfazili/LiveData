d2395d2b027b507f7d653e0be74bcb6c9c094684
Tehreem <tehreemfazili5@gmail.com>
2019-12-19 19:34:18 +0530

    initiallized viewmodel

--- app/src/main/java/com/example/admin/livedata/MainActivity.kt
+++ app/src/main/java/com/example/admin/livedata/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.admin.livedata
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        viewModel.observerText.observe(this, Observer {
+
+        })
+
     }
 }
--- app/src/main/java/com/example/admin/livedata/ViewModel.kt
+++ app/src/main/java/com/example/admin/livedata/ViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.admin.livedata
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+
+    lateinit var _observerTextState : MutableLiveData<String>  //mutable data is used to update data.. to observe data we use live data
+    val observerText : LiveData<String> // it contains the data of mutable data (it has its own method get) It is only used to get value
+     get() = _observerTextState
+
+
+    fun setValue(){
+
+        _observerTextState.value = "Text Changed"
+
+    }
+}
